a956e09d1324f6ba4309003900c5c253e3cb9643
RichiePalma <palmamendoza.5@hotmail.com>
2016-11-19 20:13:46 -0600

    Update Practica7Processing.java

--- Practica7Processing.java
+++ Practica7Processing.java
@@ -5,41 +5,30 @@ String p1="Hola",
        p2="Práctica 8",
        p3="Saludos",
        p4="Error";
-
-
 int sP1 = 2;
 int sP2 = 3;
 int sP3 = 4;
-
 color red,
       blue,
       green,
       white;
-
-
 void setup() { 
 size(800, 800);
 arduino = new Arduino(this, "COM7", 9600); 
 arduino.pinMode(sP1,Arduino.INPUT);
 arduino.pinMode(sP2,Arduino.INPUT);
 arduino.pinMode(sP3,Arduino.INPUT);
-
 red=color (255,0,0);
 blue= color (#ef8700);
 green = color (180,255,140);
 white= color (255);
 }
 void draw() { 
-
 background(red); 
 fill(255,0,127);
 rect(50,50,750,350);
 fill(255,0,127);
 rect(50,400,750,350);
-
-
-
-
 if(arduino.digitalRead(sP1)==Arduino.HIGH && arduino.digitalRead(sP2)==Arduino.LOW && arduino.digitalRead(sP3)==Arduino.LOW) {
   fill(255,0,0);
   ellipse(350,400,150,150);
